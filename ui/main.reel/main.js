0543d77ee3fe658b49009da5e8a9bd9dbc7a3be9
joshuamontage <noreply>
2015-01-08 04:09:07 +0000

    Update components

--- ui/main.reel/main.js
+++ ui/main.reel/main.js
@@ -16,7 +16,7 @@ exports.Main = Component.specialize(/** @lends Main# */ {
     },
     observableAction: {
         value: function (event) {
-            alert (event.target.identifier + " Clicked!");
+            this.delayedValue = this.delayedValue;
         }
     }
 });
